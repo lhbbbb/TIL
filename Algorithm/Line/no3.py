b4b04ededd9a79b8085b9b4b60016b2c78c59373
lhbbbb <lhb4196@gmail.com>
2019-09-23 23:48:37 +0900

    modify no3.py

--- Algorithm/Line/no3.py
+++ Algorithm/Line/no3.py
@@ -4,25 +4,7 @@ sys.stdin = open("input3.txt", 'r')
 
 T = int(input())
 
-# def search(k):
-#     if k == N:
-#         print(c)
-#     else:
-#         if time_lst[k][1] > time_lst[k + 1][0]:
-#             c[k] = 1
-#             search(k + 1)
-#         c[k] = 0
-#         search(k + 2)
-
-
 for tc in range(1, T + 1):
-    # N = int(input())
-    #
-    # time_lst = [list(map(int, input().strip().split(' '))) for _ in range(N)]
-    # time_lst = sorted(time_lst)
-    # c = [0] * N
-    # search(0)
-    # print(sorted(time_lst))
     N = int(input())
     time_lst = [list(map(int, input().strip().split(' '))) for _ in range(N)]
     lst = [[0] * 150 for _ in range(N)]
