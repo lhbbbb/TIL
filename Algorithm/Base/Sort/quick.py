7b09df1538165c7e9b2471d9da5a9be71d153247
lhbbbb <lhbbbb@github.com>
2019-10-01 09:55:35 +0900

    add sort contents and modify quick.py

--- Algorithm/Base/Sort/quick.py
+++ Algorithm/Base/Sort/quick.py
@@ -1,34 +1,40 @@
 import random
-'''
+import time
+
+"""
 https://www.daleseo.com/sort-quick/
-'''
-rand_lst = random.sample(range(20), 20)
+"""
+
+random_lst = random.sample(range(100000), 100000)
+
+start = time.time()
 
 
-def partition(low, high, lst):
-    pivot = lst[(low + high) // 2]
-    while low <= high:
-        # 좌측 찾기
-        while lst[low] < pivot:
-            low += 1
-        # 우측 찾기
-        while lst[high] > pivot:
-            high -= 1
-        if low <= high:
-            lst[low], lst[high] = lst[high], lst[low]
-            low += 1
-            high -= 1
-    return low
-
-
-# Quick Sort
-def quicksort(left, right, lst):
-    if left >= right:
+def quick_sort(lst, l, r):
+    if r <= l:
         return
-    mid = partition(left, right, lst)
-    quicksort(left, mid - 1, lst)
-    quicksort(mid, right, lst)
+
+    def partition(low, high):
+        pivot = lst[(low + high) // 2]
+
+        while low <= high:
+            while lst[low] < pivot:
+                low += 1
+            while lst[high] > pivot:
+                high -= 1
+            if low <= high:
+                lst[low], lst[high] = lst[high], lst[low]
+                low += 1
+                high -= 1
+        return low, high
+
+    right_start, left_end = partition(l, r)
+    quick_sort(lst, l, left_end)
+    quick_sort(lst, right_start, r)
+
+    return lst
 
 
-quicksort(0, len(rand_lst) - 1, rand_lst)
-print(rand_lst)
+quick_sort(random_lst, 0, len(random_lst) - 1)
+# sorted(random_lst)
+print(time.time() - start)
