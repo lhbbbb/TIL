32ec664f3600e35998d8564b39411135b5792cd1
lhbbbb <lhb4196@gmail.com>
2019-11-06 09:03:16 +0900

    modify 17825

--- Algorithm/Baekjoon/주사위윷놀이/17825.py
+++ Algorithm/Baekjoon/주사위윷놀이/17825.py
@@ -1,51 +1,49 @@
-# import sys
-
-# sys.stdout = open('output.txt', 'w')
-
 lst = list(map(int, input().split()))
 
-board = [0] * 500
+board = [0] * 501
 board_dict = {}
 
 
-def backtrack(k, score, player_dict):
+def backtrack(k):
     global max_val
     if k == 10:
+        player = [0] * 5
+        player_loc = [0] * 5
+        for i in range(10):
+            loc = player_loc[visited[i]] + lst[i]
+            if 0 <= loc <= 19 or 100 <= loc <= 103 or 200 <= loc <= 202 or 300 <= loc <= 303 or 400 <= loc <= 403:
+                if loc == 5:
+                    loc = 100
+                elif loc == 10:
+                    loc = 200
+                elif loc == 15:
+                    loc = 300
+                if loc in player_loc:
+                    return
+                player_loc[visited[i]] = loc
+            else:
+                if loc == 20:
+                    loc = 403
+                elif 104 <= loc <= 107:
+                    loc = 400 + (loc - 104)
+                elif 203 <= loc <= 206:
+                    loc = 400 + (loc - 203)
+                elif 304 <= loc <= 307:
+                    loc = 400 + (loc - 304)
+                player_loc[visited[i]] = loc
+                board_dict[player_loc[visited[i]]] = 0
+            player[visited[i]] += board_dict[player_loc[visited[i]]]
+        score = sum(player)
         if score > max_val:
             max_val = score
-        # print(visited)
     else:
         for i in range(1, 5):
-            if player[i]:
-                continue
-            new_player_dict = player_dict.copy()
             visited[k] = i
-            new_player_dict[i] += lst[k]
-            if 1 <= new_player_dict[i] <= 20 or 101 <= new_player_dict[i] <= 107 or 201 <= new_player_dict[
-                i] <= 206 or 301 <= \
-                    new_player_dict[i] <= 307:
-                if not board[new_player_dict[i]]:
-                    if new_player_dict[i] == 5:
-                        new_player_dict[i] = 100
-                    elif new_player_dict[i] == 10:
-                        new_player_dict[i] = 200
-                    elif new_player_dict[i] == 15:
-                        new_player_dict[i] = 300
-                    board[new_player_dict[i]] = True
-                else:
-                    player[i] = 0
-                    board[new_player_dict[i]] = 0
-                    return
-            else:
-                player[i] = True
-                board_dict[new_player_dict[i]] = 0
-            new_score = score + board_dict[new_player_dict[i]]
-            backtrack(k + 1, new_score, new_player_dict)
-            player[i] = 0
+            backtrack(k + 1)
 
 
 # normal route
-for i in range(1, 21):
+for i in range(1, 20):
     board_dict[i] = i * 2
 
 # 10 route
@@ -55,11 +53,6 @@ for i in range(101, 104):
     board_dict[i] = num
     num += 3
 
-num = 25
-for i in range(104, 108):
-    board_dict[i] = num
-    num += 5
-
 # 20 route
 num = 22
 board_dict[200] = 20
@@ -67,29 +60,21 @@ for i in range(201, 203):
     board_dict[i] = num
     num += 2
 
-num = 25
-for i in range(203, 207):
-    board_dict[i] = num
-    num += 5
-
 # 30 route
 num = 28
 board_dict[300] = 30
-for i in range(301, 305):
+for i in range(301, 304):
     board_dict[i] = num
     num -= 1
 
-num = 30
-for i in range(305, 308):
+# 25~40 route
+num = 25
+for i in range(400, 404):
     board_dict[i] = num
     num += 5
 
-print(board_dict)
 max_val = 0
 visited = [0] * 10
-player = [0] * 5
-player_dict = {}
-for i in range(1, 5):
-    player_dict[i] = 0
-backtrack(0, 0, [0, 0, 0, 0, 0])
+cnt = 0
+backtrack(0)
 print(max_val)
